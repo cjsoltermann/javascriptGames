a9a7289ad8d193d4618724011edb0be980f4201a
Christian Soltermann <applesauce.soltermann@gmail.com>
2018-11-23 13:45:17 -0600

    Rotation

--- squares/game.js
+++ squares/game.js
@@ -1,48 +1,64 @@
-var boxSize = 10;
-var width = 500;
-var gameSpeed = 10;
-var boxes = [];
-var canvas, ctx;
-var player1, player2;
-var keys = {};
-var keyFuncs = {};
-var mouseX;
-var mouseY;
-var canvasX, canvasY;
-
-var Box = {}
+var Box = {
+  boxSize: 10,
+  width: 500,
+  gameSpeed: 10,
+  boxes: [],
+  canvas: {}, ctx: {},
+  player1: {}, player2: {},
+  keys: {},
+  controllers: [],
+  keyFuncs: {},
+  mouseX: {}, mouseY: {},
+  canvasX: {}, canvasY: {}
+}
 
 Box.box = function(x, y, color, size) {
   this.x = x ? x : 0;
   this.y = y ? y : 0;
   this.color = color ? color : "red";
   this.size = size ? size : 1;
+  this.rotation = 0;
+  this.center = [0, 0];
 
   this.draw = function(x, y) {
-    ctx.fillStyle = this.color;
-    var size = this.size * boxSize;
-    ctx.fillRect((x ? x : 0) + this.x, (y ? y : 0) + this.y, size, size);
+    Box.ctx.fillStyle = this.color;
+    var size = this.size * Box.boxSize;
+
+    var r = this.rotation % (Math.PI * 2);
+    if (r != 0) {
+      Box.ctx.save();
+      Box.ctx.translate(this.x + this.center[0], this.y + this.center[1]);
+      Box.ctx.rotate(r);
+      Box.ctx.translate(-this.center[0], -this.center[1]);
+      Box.ctx.fillRect(0, 0, size, size);
+      Box.ctx.restore();
+      return;
+    }
+
+    Box.ctx.fillRect((x ? x : 0) + this.x, (y ? y : 0) + this.y, size, size);
   }
 }
 
 function draw() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0,0,width,width);
-  for (let box of boxes) {
+  Box.ctx.fillStyle = "white";
+  Box.ctx.fillRect(0,0,Box.width,Box.width);
+  for (let box of Box.boxes) {
     box.draw();
   }
 }
 
-Box.physicsBox = function(x, y, color, size, vx, vy) {
+Box.physicsBox = function(x, y, color, size, vx, vy, vr) {
   Box.box.call(this, x, y, color, size);
   this.vx = vx ? vx : 0;
   this.vy = vy ? vy : 0;
+  this.vr = vr ? vr : 0;
   
   this.update = function () {};
   this._update = function() {
     this.update();
     this.x += this.vx;
     this.y += this.vy;
+    this.rotation += this.vr;
   }
 }
 
@@ -62,41 +78,41 @@ Box.collection = function(x, y, boxes) {
   }
 
   this.addBoxes = function() {
-    for (argument of arguments)
+    for (let argument of arguments)
       this.boxes.push(argument);
   }
 }
 
 function addBox(box) {
-  boxes.push(box);
+  Box.boxes.push(box);
 }
 
 function addBoxes() {
-  for (argument of arguments)
-    boxes.push(argument);
+  for (let argument of arguments)
+    Box.boxes.push(argument);
 }
 
 function removeBox(box) {
-  var index = boxes.indexOf(box);
+  var index = Box.boxes.indexOf(box);
   if (index !== -1)
-    boxes.splice(index, 1);
+    Box.boxes.splice(index, 1);
 }
 
 function removeBoxes() {
-  for (arg of arguments)
+  for (let arg of arguments)
     removeBox(arg);
 }
 
 function setup() {
-  canvas = document.getElementById("game");
-  ctx = canvas.getContext("2d");
-  canvas.width = width;
-  canvas.height = width;
-  canvasX = canvas.getBoundingClientRect().left;
-  canvasY = canvas.getBoundingClientRect().top;
-  document.addEventListener("keydown", function(e) { keys[e.key] = 1; }, false);
-  document.addEventListener("keyup", function(e) { keys[e.key] = 0; }, false);
-  canvas.addEventListener("mousemove", function(e) { mouseX = e.clientX - canvasX; mouseY = e.clientY - canvasY; }, false);
+  Box.canvas = document.getElementById("game");
+  Box.ctx = Box.canvas.getContext("2d");
+  Box.canvas.width = Box.width;
+  Box.canvas.height = Box.width;
+  Box.canvasX = Box.canvas.getBoundingClientRect().left;
+  Box.canvasY = Box.canvas.getBoundingClientRect().top;
+  document.addEventListener("keydown", function(e) { Box.keys[e.key] = 1; }, false);
+  document.addEventListener("keyup", function(e) { Box.keys[e.key] = 0; }, false);
+  Box.canvas.addEventListener("mousemove", function(e) { Box.mouseX = e.clientX - Box.canvasX; Box.mouseY = e.clientY - Box.canvasY; }, false);
 }
 
 function safeMove(object, x, y) {
@@ -112,7 +128,7 @@ function safeMove(object, x, y) {
 
 function shootBullet(src, x, y) {
   if (!src.coolDown > 0) {
-    var bullet = new Box.physicsBox(src.x + 30, src.y, "yellow", 0.5, x, y);
+    var bullet = new Box.physicsBox(src.x + 30, src.y, "black", 0.5, x, y, 0.1);
     addBox(bullet);
     bullet.update = function() {if (offScreen(this)) { removeBox(this) }};
     src.coolDown = 10;
@@ -120,61 +136,86 @@ function shootBullet(src, x, y) {
   src.coolDown--;
 }
 
+function controller(box, up, down, left, right) {
+  if (Box.keys[up])
+    safeMove(box, 0, -1);
+  else if (Box.keys[down])
+    safeMove(box, 0, 1);
+  
+  if (Box.keys[left])
+    safeMove(box, -1, 0);
+  else if (Box.keys[right])
+    safeMove(box, 1, 0);
+}
+
 function main() {
   setup();
 
-  player1 = new Box.collection(5, 5);
-  player1.addBox(new Box.box(0, 0));
-  player1.size = 1;
-  onKeyDown("w", function () { safeMove(player1, 0, -1); })
-  onKeyDown("a", function () { safeMove(player1, -1, 0); })
-  onKeyDown("s", function () { safeMove(player1, 0, 1); })
-  onKeyDown("d", function () { safeMove(player1, 1, 0); })
-  onKeyDown("f", function () { shootBullet(player1, 2, 0); });
+  Box.player1 = new Box.collection(5, 5);
+  Box.player1.addBox(new Box.box(0, 0));
+  Box.player1.size = 1;
+  addController("w s a d", function() { controller(Box.player1, "w", "s", "a", "d") });
+  onKeyDown("f", function (key) { shootBullet(Box.player1, 2, 0); });
 
-  player2 = new Box.box(25, 25, "blue");
-  onKeyDown("ArrowUp", function () { safeMove(player2, 0, -1); });
-  onKeyDown("ArrowLeft", function () { safeMove(player2, -1, 0); });
-  onKeyDown("ArrowDown", function () { safeMove(player2, 0, 1); });
-  onKeyDown("ArrowRight", function () { safeMove(player2, 1, 0); });
-  onKeyDown("1", function () { shootBullet(player2, 1, 0); });
+  Box.player2 = new Box.box(25, 25, "blue");
+  addController("ArrowUp ArrowDown ArrowLeft ArrowRight", function() { controller(Box.player2, "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight") });
+  onKeyDown("1", function (key) { shootBullet(Box.player2, 1, 0); });
 
   collectionTest = new Box.collection(20, 0);
   collectionTest.addBox(new Box.box(2.5, 0, "gray", .6));
   collectionTest.addBox(new Box.box(-2.5, 0, "gray", .6));
-  player1.addBox(collectionTest);
+  Box.player1.addBox(collectionTest);
 
-  addBoxes(player1, player2);
+  Box.rotationTest = new Box.box(200, 200, "red", 5);
+  Box.rotationTest.center = [25, 25];
+  onKeyDown("p", function () { Box.rotationTest.rotation += .1} );
+
+  addBoxes(Box.player1, Box.player2, Box.rotationTest);
   gameLoop();
 }
 
 function onKeyDown(key, func) {
-  keyFuncs[key] = func;
+  var keys = key.split(" ");
+  for (let k in keys)
+    Box.keyFuncs[keys[k]] = func;
+}
+
+function addController(keys, func) {
+  var controller = { keys: keys.split(" "), func: func };
+  Box.controllers.push(controller);
 }
 
 function processKeys() {
-  for (key in keys) {
-    if (keys[key] && keyFuncs[key])
-      keyFuncs[key]();
+  for (let key in Box.keys) {
+    if (Box.keys[key] && Box.keyFuncs[key])
+      Box.keyFuncs[key](key);
+  }
+  for (let c in Box.controllers) {
+    for (let key in Box.controllers[c].keys) {
+      if (Box.keys[Box.controllers[c].keys[key]]) {
+        Box.controllers[c].func();
+        break;
+      }
+    }
   }
 }
 
 function gameLoop() {
   update();
   draw();
-  setTimeout(gameLoop, 10 / gameSpeed);
+  setTimeout(gameLoop, 10 / Box.gameSpeed);
 }
 
 function offScreen(thing) {
-  size = thing.size ? thing.size * boxSize : 0;
-  if (thing.x > width - size || thing.x < 0 || thing.y > width - size || thing.y < 0)
+  size = thing.size ? thing.size * Box.boxSize : 0;
+  if (thing.x > Box.width - size || thing.x < 0 || thing.y > Box.width - size || thing.y < 0)
     return true;
   return false;
 }
 
 function update() {
   processKeys();
-  for (let box of boxes)
+  for (let box of Box.boxes)
     if (box._update)
       box._update()
 }
